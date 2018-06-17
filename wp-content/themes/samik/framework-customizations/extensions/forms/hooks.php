ea39ed82737e2d6d8f9f134984a1a7d0527f9695
AvetikAtoian <avetik@pindrop.nl>
2018-06-17 15:59:51 +0300

    improvements; add iconed-radio and iconed-checkboxes items;

--- wp-content/themes/samik/framework-customizations/extensions/forms/hooks.php
+++ wp-content/themes/samik/framework-customizations/extensions/forms/hooks.php
@@ -5,5 +5,7 @@ function _action_theme_fw_ext_forms_include_custom_builder_items() {
     require_once dirname(__FILE__) .'/includes/builder-items/card-section/class-fw-option-type-form-builder-item-card-section.php';
     require_once dirname(__FILE__) .'/includes/builder-items/date-picker/class-fw-option-type-form-builder-item-date-picker.php';
     require_once dirname(__FILE__) .'/includes/builder-items/time-picker/class-fw-option-type-form-builder-item-time-picker.php';
+    require_once dirname(__FILE__) .'/includes/builder-items/iconed-checkboxes/class-fw-option-type-form-builder-item-iconed-checkboxes.php';
+    require_once dirname(__FILE__) .'/includes/builder-items/iconed-radio/class-fw-option-type-form-builder-item-iconed-radio.php';
 }
 add_action('fw_option_type_form_builder_init', '_action_theme_fw_ext_forms_include_custom_builder_items');
---
+++ wp-content/themes/samik/framework-customizations/extensions/forms/includes/builder-items/iconed-checkboxes/view.php
@@ -0,0 +1,81 @@
+<?php if (!defined('FW')) die('Forbidden');
+/**
+ * @var array $item
+ * @var array $choices
+ * @var array $value
+ */
+
+$options = $item['options'];
+
+switch ($options['layout']) {
+	case 'one-column': $columns = 1; break;
+	case 'two-columns': $columns = 2; break;
+	case 'three-columns': $columns = 3; break;
+	default: $columns = 0;
+}
+?>
+<?php if (empty($choices)): ?>
+	<!-- checkboxes not displayed: no choices -->
+<?php else: ?>
+	<div class="<?php echo esc_attr(fw_ext_builder_get_item_width('form-builder', $item['width'] .'/frontend_class')) ?>">
+		<div class="form-group">
+			<?php if ($options['label']): ?>
+                <div class="form-group_left">
+                    <label><?php echo fw_htmlspecialchars($options['label']) ?>
+                        <?php if ($options['required']): ?><sup>*</sup><?php endif; ?>
+                    </label>
+                </div>
+                <div class="form-group_right">
+			<?php endif; ?>
+
+                <div class="field-columns-<?php echo esc_attr($columns); ?> form-icon-list">
+                    <?php if ($columns > 1): ?>
+                        <?php
+                        $choices_count = count($choices);
+                        $choices_per_column = abs($choices_count / $columns)
+                                              + (($choices_count % $columns > $columns / 2) ? 1 : 0);
+                        $counter = 0;
+                        ?>
+                        <div class="field-column">
+                        <?php while ($choice = array_shift($choices)): ?>
+                            <?php
+                                $choice['attr']['id'] = 'rand-'. fw_unique_increment();
+                                $choice_attrs = $choice['attr'];
+                                $choice_options = $choice['options']; ?>
+                            <div class="form-icon">
+                                <input class="form-icon-input" <?php echo fw_attr_to_html($choice_attrs) ?> />
+                                <label class="form-icon-label tipso tipso_style <?php echo $choice_options['icon']['icon-class-without-root'] ?>"
+                                       <?php if ($choice_options['hint-title']) { ?>data-tipso-title="<?php echo $choice_options['hint-title']; } ?>"
+                                       <?php if ($choice_options['hint-content']) { ?>data-tipso="<?php echo $choice_options['hint-content']; } ?>"
+                                       for="<?php echo esc_attr($choice_attrs['id']) ?>"></label>
+                            </div>
+                            <?php if (!(++$counter % $choices_per_column)): ?>
+                                </div><div class="field-column">
+                            <?php endif; ?>
+                        <?php endwhile; ?>
+                        </div>
+                    <?php else: ?>
+                        <?php foreach ($choices as $choice):
+                            $choice['attr']['id'] = 'rand-'. fw_unique_increment();
+                            $choice_attrs = $choice['attr'];
+                            $choice_options = $choice['options']; ?>
+                            <div class="form-icon">
+                                <input class="form-icon-input" <?php echo fw_attr_to_html($choice_attrs) ?> />
+                                <label class="form-icon-label tipso tipso_style <?php echo $choice_options['icon']['icon-class-without-root'] ?>"
+                                       <?php if ($choice_options['hint-title']) { ?>data-tipso-title="<?php echo $choice_options['hint-title']; } ?>"
+                                       <?php if ($choice_options['hint-content']) { ?>data-tipso="<?php echo $choice_options['hint-content']; } ?>"
+                                       for="<?php echo esc_attr($choice_attrs['id']) ?>"></label>
+                            </div>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </div>
+                <?php if ($options['info']): ?>
+                    <p><em><?php echo $options['info'] ?></em></p>
+                <?php endif; ?>
+
+            <?php if ($options['label']): ?>
+                </div>
+            <?php endif; ?>
+		</div>
+	</div>
+<?php endif; ?>
