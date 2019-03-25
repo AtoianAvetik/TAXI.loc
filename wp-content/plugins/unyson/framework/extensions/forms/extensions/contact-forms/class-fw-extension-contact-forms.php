8a5838f2dce83e8eb0652685ea793f321a24f3f6
AvetikAtoian <avetik@pindrop.nl>
2019-03-25 21:34:15 +0200

    remove print

--- wp-content/plugins/unyson/framework/extensions/forms/extensions/contact-forms/class-fw-extension-contact-forms.php
+++ wp-content/plugins/unyson/framework/extensions/forms/extensions/contact-forms/class-fw-extension-contact-forms.php
@@ -61,8 +61,6 @@ class FW_Extension_Contact_Forms extends FW_Extension_Forms_Form {
 			return '';
 		}
 
-		fw_print($data);
-
 		$form_id = $data['id'];
 		$submit_button_text = empty( $data['submit_button_text'] )
 			? __( 'Submit', 'fw' )
